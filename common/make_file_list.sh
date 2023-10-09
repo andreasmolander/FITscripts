132ccc56d4374dae3ee69f26b217ba06965a8a95
Andreas Molander <andreas.molander@cern.ch>
2023-10-09 11:11:24 +0200

    Generalize make_file_list.sh

--- common/make_file_list.sh
+++ common/make_file_list.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-# script to generate file list from alien, only data CTFs at the moment
-
-# if [ ! "$#" -eq 3 ]; then
-#     echo "Usage: $0 <year> <lhcperiod> <runNumber>"
-#     exit 1
-# fi
-
-# year="$1"
-# lhcperiod="$2"
-# runnumber="$3"
-
-year=2023
-lhcperiod=LHC23zze
-runnumber=543950
-
-nfiles=5000
+# script to generate file list from alien, only data raw/calib CTFs at the moment
+set -x
+year=
+lhcperiod=
+runnumber=
+type="raw"
+nfiles=-1
 filename=file.lst
 
-alien_find "/alice/data/${year}/${lhcperiod}/${runnumber}/raw o2_ctf_run*.root" > $filename
+Usage()
+{
+    echo "Usage instructions not implemented"
+}
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        -y) year=$2; shift 2;;
+        -p) lhcperiod=$2; shift 2;;
+        -r) runnumber=$2; shift 2;;
+        -t) type=$2; shift 2;;
+        -n) nfiles=$2; shift 2;;
+        -f) filename=$2; shift 2;;
+        *) echo "Wrong input"; Usage;
+    esac
+done
+
+alien_find "/alice/data/${year}/${lhcperiod}/${runnumber}/${type} o2_ctf_run*.root" > $filename
 echo "Total number of files = $(cat $filename | wc -l)"
-echo "Picking ${nfiles} randomly"
-shuf -n $nfiles $filename -o $filename
+if [ $nfiles -gt -1 ]; then
+    echo "Picking ${nfiles} randomly"
+    shuf -n $nfiles $filename -o $filename
+fi
 
 sed -i -e 's/^/alien\:\/\//' $filename
