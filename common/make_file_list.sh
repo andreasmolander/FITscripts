cd5448a5acbc365b977c4992df8340688ba01860
Andreas Molander <andreas.molander@cern.ch>
2024-01-03 17:47:21 +0200

    Minor usability fixes to make_file_list.sh

--- common/make_file_list.sh
+++ common/make_file_list.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# script to generate file list from alien, only data raw/calib CTFs at the moment
+# script to generate file list from alien
 # 
-# Usage: ./make_file_list.sh -y <year> -p <lhcperiod> -r <runnumber> -t <type> -n <nfiles> -f <filename>
+# Usage: ./make_file_list.sh -y <year> -p <lhcperiod> -r <runnumber> -t <type> [-n <nfiles> -f <filename>]
 
-set -x # Debug
+# set -x # Debug
 
 year=""
 lhcperiod=""
@@ -19,7 +19,7 @@ Usage()
 	echo "  -y, --year YEAR      year"
 	echo "  -p, --period PERIOD  LHC period"
 	echo "  -r, --run RUN        run number"
-	echo "  -t, --type TYPE      file type (raw, calib)"
+	echo "  -t, --type TYPE      file type. Options: raw, calib, [pass], mc. raw and calib are data CTFs. [pass] is data AO2Ds from [pass]. mc is MC AO2Ds."
 	echo "  -n, --nfiles NFILES  number of files to pick randomly"
 	echo "  -f, --filename FILE  output file name"
 	echo "  -h, --help           print this help"
@@ -28,34 +28,51 @@ Usage()
 
 while [ $# -gt 0 ]; do
 	case $1 in
-		-y) year=$2; shift 2;;
-		-p) lhcperiod=$2; shift 2;;
-		-r) runnumber=$2; shift 2;;
-		-t) type=$2; shift 2;;
-		-n) nfiles=$2; shift 2;;
-		-f) filename=$2; shift 2;;
-		-h|--help) Usage;;
+		-y | --year)     year=$2;      shift 2;;
+		-p | --period)   lhcperiod=$2; shift 2;;
+		-r | --run)      runnumber=$2; shift 2;;
+		-t | --type)     type=$2;      shift 2;;
+		-n | --nfiles)   nfiles=$2;    shift 2;;
+		-f | --filename) filename=$2;  shift 2;;
+		-h | --help)     Usage;;
 		*) echo "Wrong input"; Usage;
 	esac
 done
 
-if [ $type == "raw" ] || [ $type == "calib" ]; then
+# TODO: is it safe to leave out period for all but MC?
+if [ "$year" == "" ] || [ "$lhcperiod" == "" ] || [ "$runnumber" == "" ] || [ "$type" == "" ]; then
+	echo "Missing arguments"
+	Usage
+	exit 1
+fi
+
+if [ "$type" == "raw" ] || [ "$type" == "calib" ]; then
 	echo "Looking for CTF files"
 	if [ -z $lhcperiod ]; then
 		alien_find "/alice/data/${year}/ ${runnumber}/${type}/*/o2_ctf_run*.root" > $filename    
 	else
 		alien_find "/alice/data/${year}/${lhcperiod}/${runnumber}/${type} o2_ctf_run*.root" > $filename
 	fi
-elif [ $type == "mc" ]; then
+elif [ "$type" == "mc" ]; then
 	echo "Looking for MC files"
 	alien_find "/alice/sim/${year}/${lhcperiod}/ ${runnumber}/*/AO2D.root" > $filename
 else
-	# Asume it's AO2D then
+	# Asume it's data AO2D then
 	echo "Looking for AO2D files"
-	alien_find "/alice/data/${year}/${lhcperiod}/${runnumber}/${type} AO2D*.root" > $filename
+	# Should we look for AO2D_merged.root?
+	if [ -z $lhcperiod ]; then
+		alien_find "/alice/data/${year}/ ${runnumber}/${type}/*/AO2D.root" > $filename
+	else
+		alien_find "/alice/data/${year}/${lhcperiod}/${runnumber}/${type} AO2D.root" > $filename
+	fi
 fi
 
 echo "Total number of files = $(cat $filename | wc -l)"
+
+if [ $(cat $filename | wc -l) -eq 0 ]; then
+	echo "No files found"
+	exit 1
+fi
 
 if [ $nfiles -gt -1 ]; then
 	echo "Picking ${nfiles} randomly"
